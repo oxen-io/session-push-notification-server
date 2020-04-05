b5d6298566348e76d910030377b30a6b81ae607d
ryanzhao <ryanzhaors@qq.com>
2020-04-06 09:24:44 +1000

    update the process for expiration more than one day

--- utils.py
+++ utils.py
@@ -2,8 +2,11 @@ import time
 
 
 def process_expiration(expiration):
+    # The expiration of a friend request message can be 4 days,
+    # this method will process the expiration for friend request messages,
+    # to make it like the expiration is one day.
     current_time = int(round(time.time() * 1000))
-    ms_of_a_day = 24 * 60 *60 * 1000
-    while expiration - current_time > ms_of_a_day:
-        expiration -= ms_of_a_day
+    ms_of_a_day = 24 * 60 * 60 * 1000
+    if expiration - current_time > ms_of_a_day:
+        expiration -= 3 * ms_of_a_day
     return expiration
