95044e17690347cb52cc2d606037c4a7b7a9c6c4
ryanzhao <ryanzhaors@qq.com>
2020-04-07 09:25:30 +1000

    minor crash fix

--- lokiAPI.py
+++ lokiAPI.py
@@ -172,6 +172,8 @@ class LokiAPI:
             if data is None:
                 continue
             message_json = json.loads(data['body'])
+            if not message_json or 'messages' not in dict(message_json).keys():
+                continue
             messages = list(message_json['messages'])
             old_length = len(messages_dict[pubkey_list[pubkey_index]])
             new_length = len(messages)
