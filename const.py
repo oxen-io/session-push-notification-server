5773ea2c7f23b79c33aaff8d59dab0b8ad10c082
Ryan ZHAO <ryanzhaors@qq.com>
2020-09-21 16:56:23 +1000

    merge original codes for PN approach migrating

--- const.py
+++ const.py
@@ -20,7 +20,7 @@ NONCE_LENGTH = 12
 TAG_LENGTH = 16
 
 TOKEN_DB = 'token_db'
-PUBKEY_TOKEN_DB = 'token_pubkey_db'
+PUBKEY_TOKEN_DB_V2 = 'token_pubkey_db_v2'
 CLOSED_GROUP_DB = 'closed_group_db'
 CERT_FILE = 'cert.pem'
 PRIVKEY_FILE = 'x25519-priv.pem'
@@ -29,4 +29,19 @@ FIREBASE_TOKEN = 'loki-5a81e-firebase-adminsdk-7plup-0698317995.json'
 BUNDLE_ID = 'com.loki-project.loki-messenger'
 
 debug_mode = True
+
+# PN approach V1 #
+LASTHASH = 'lastHash'
+EXPIRATION = 'expiration'
+
+PUBKEY_TOKEN_DB = 'token_pubkey_db'
+LAST_HASH_DB = 'last_hash_db'
+SWARM_DB = 'swarm_db'
+
+minimumSnodeCount = 2
+targetSnodeCount = 3
+maxRandomSnodePoolSize = 1024
+failureThreshold = 2
+apiVersion = 'v1'
+defaultTimeout = 10
 
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from pushNotificationHandler import PushNotificationHelper
+from pushNotificationHandler import PushNotificationHelperV2
 from const import *
 from lokiLogger import LokiLogger
 import urllib3
@@ -10,16 +10,96 @@ import resource
 from utils import decrypt, encrypt, make_symmetric_key
 import json
 
-
 resource.setrlimit(resource.RLIMIT_NOFILE, (65536, 65536))
 urllib3.disable_warnings()
 
 app = Flask(__name__)
 logger = LokiLogger().logger
-PN_helper = PushNotificationHelper(logger)
+
+# PN approach V1 #
+from pushNotificationHandler import SilentPushNotificationHelper, NormalPushNotificationHelper
+
+SPN_helper = SilentPushNotificationHelper(logger)
+NPN_helper = NormalPushNotificationHelper(logger)
 
 
-def register(args):
+@app.route('/register', methods=[GET, POST])
+def register():
+    token = None
+    pubkey = None
+    response = jsonify({CODE: 0,
+                        MSG: PARA_MISSING})
+
+    if TOKEN in request.args:
+        token = request.args[TOKEN]
+        if PUBKEY in request.args:
+            pubkey = request.args[PUBKEY]
+
+    if request.json and TOKEN in request.json:
+        token = request.json[TOKEN]
+        if PUBKEY in request.json:
+            pubkey = request.json[PUBKEY]
+
+    if request.form and TOKEN in request.form:
+        token = request.form[TOKEN]
+        if PUBKEY in request.form:
+            pubkey = request.form[PUBKEY]
+
+    if token and pubkey:
+        NPN_helper.update_token_pubkey_pair(token, pubkey)
+        SPN_helper.disable_token(token)
+        response = jsonify({CODE: 1,
+                            MSG: SUCCESS})
+    elif token:
+        SPN_helper.update_token(token)
+        NPN_helper.disable_token(token)
+        response = jsonify({CODE: 1,
+                            MSG: SUCCESS})
+    return response
+
+
+@app.route('/acknowledge_message_delivery', methods=[GET, POST])
+def update_last_hash():
+    last_hash = None
+    pubkey = None
+    expiration = None
+    response = jsonify({CODE: 0,
+                        MSG: PARA_MISSING})
+
+    if LASTHASH in request.args:
+        last_hash = request.args[LASTHASH]
+        if PUBKEY in request.args:
+            pubkey = request.args[PUBKEY]
+        if EXPIRATION in request.args:
+            expiration = request.args[EXPIRATION]
+
+    if request.json and LASTHASH in request.json:
+        last_hash = request.json[LASTHASH]
+        if PUBKEY in request.json:
+            pubkey = request.json[PUBKEY]
+        if EXPIRATION in request.json:
+            expiration = request.json[EXPIRATION]
+
+    if request.form and LASTHASH in request.form:
+        last_hash = request.form[LASTHASH]
+        if PUBKEY in request.form:
+            pubkey = request.form[PUBKEY]
+        if EXPIRATION in request.form:
+            expiration = request.form[EXPIRATION]
+
+    if last_hash and pubkey and expiration:
+        NPN_helper.update_last_hash(pubkey, last_hash, expiration)
+        response = jsonify({CODE: 1,
+                            MSG: SUCCESS})
+
+    return response
+
+
+# PN approach V2 #
+PN_helper_v2 = PushNotificationHelperV2(logger)
+
+
+def register_v2(args):
     device_token = None
     session_id = None
     if TOKEN in args:
@@ -28,7 +108,10 @@ def register(args):
         session_id = args[PUBKEY]
 
     if device_token and session_id:
-        PN_helper.register(device_token, session_id)
+        PN_helper_v2.register(device_token, session_id)
+        # PN approach V1 #
+        NPN_helper.update_token_pubkey_pair(device_token, session_id)
+        SPN_helper.disable_token(device_token)
     else:
         raise Exception(PARA_MISSING)
 
@@ -39,7 +122,10 @@ def unregister(args):
         device_token = args[TOKEN]
 
     if device_token:
-        PN_helper.unregister(device_token)
+        PN_helper_v2.unregister(device_token)
+        # PN approach V1 #
+        SPN_helper.update_token(device_token)
+        NPN_helper.disable_token(device_token)
     else:
         raise Exception(PARA_MISSING)
 
@@ -53,7 +139,7 @@ def subscribe_closed_group(args):
         closed_group_id = args[CLOSED_GROUP]
 
     if closed_group_id and session_id:
-        PN_helper.subscribe_closed_group(closed_group_id, session_id)
+        PN_helper_v2.subscribe_closed_group(closed_group_id, session_id)
     else:
         raise Exception(PARA_MISSING)
 
@@ -67,7 +153,7 @@ def unsubscribe_closed_group(args):
         closed_group_id = args[CLOSED_GROUP]
 
     if closed_group_id and session_id:
-        PN_helper.unsubscribe_closed_group(closed_group_id, session_id)
+        PN_helper_v2.unsubscribe_closed_group(closed_group_id, session_id)
     else:
         raise Exception(PARA_MISSING)
 
@@ -81,12 +167,12 @@ def notify(args):
         data = args[DATA]
 
     if session_id and data:
-        PN_helper.add_message_to_queue(args)
+        PN_helper_v2.add_message_to_queue(args)
     else:
         raise Exception(PARA_MISSING)
 
 
-Routing = {'register': register,
+Routing = {'register': register_v2,
            'unregister': unregister,
            'subscribe_closed_group': subscribe_closed_group,
            'unsubscribe_closed_group': unsubscribe_closed_group,
@@ -129,10 +215,13 @@ def onion_request():
 
 
 if __name__ == '__main__':
-    PN_helper.run()
-    port = 4000  # for stage server
+    SPN_helper.run()
+    NPN_helper.run()
+    PN_helper_v2.run()
+    port = 3000 if debug_mode else 5000
     http_server = HTTPServer(WSGIContainer(app), no_keep_alive=True)
     http_server.listen(port)
     IOLoop.instance().start()
-    PN_helper.stop()
-
+    SPN_helper.stop()
+    NPN_helper.stop()
+    PN_helper_v2.stop()
