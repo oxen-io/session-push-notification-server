741adde387fafcdc75d43ed1ada9eadcb88a45dc
ryanzhao <ryanzhaors@qq.com>
2023-01-31 14:17:35 +1100

    fix the issue where task future not in the same runLoop

--- tasks/pushNotificationTask.py
+++ tasks/pushNotificationTask.py
@@ -5,10 +5,11 @@ from tools.pushNotificationHandler import PushNotificationHelperV2
 class PushNotificationTask(BaseTask):
     def __init__(self):
         super().__init__()
-
-        self.notification_helper = PushNotificationHelperV2()
+        self.notification_helper = None # Create the instance in the runLoop
 
     async def task(self):
+        self.notification_helper = PushNotificationHelperV2()
+
         while self.is_running:
             await self.notification_helper.send_push_notification()
             await asyncio.sleep(0.5)
