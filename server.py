1db55c1940e10302550c9a902d396a031f81d337
ryanzhao <ryanzhaors@qq.com>
2023-06-20 11:03:28 +1000

    fix extra json loads

--- server.py
+++ server.py
@@ -91,7 +91,7 @@ def register_legacy_groups_only(args):
     else:
         device_type = DeviceType.iOS if is_ios_device_token(device_token) else DeviceType.Android
     if HTTP.SubscriptionRequest.CLOSED_GROUPS in args:
-        closed_group_ids = json.loads(args[HTTP.SubscriptionRequest.CLOSED_GROUPS])
+        closed_group_ids = args[HTTP.SubscriptionRequest.CLOSED_GROUPS]
 
     if device_token and session_id and closed_group_ids:
         PushNotificationHelperV2().register_legacy_groups_only(device_token, session_id, device_type, closed_group_ids)
